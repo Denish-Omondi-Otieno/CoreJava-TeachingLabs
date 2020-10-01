08796f85968a061e613f17eb8f6cc87b82ed2f21
ADMIN <ADMIN@Academy>
2020-10-01 16:42:44 -0500

    JOptionPane String for Text and Title

---
+++ CoreJavaTeachingLabs/src/chapter_1/L15_WelcomeInMessageDialogBox.java
@@ -0,0 +1,12 @@
+package chapter_1;
+
+import javax.swing.JOptionPane;
+
+public class L15_WelcomeInMessageDialogBox {
+	public static void main (String [] args) {
+		JOptionPane.showMessageDialog(null, "String for the Text", "String for the Title", JOptionPane.INFORMATION_MESSAGE);
+		
+	}
+
+}
+

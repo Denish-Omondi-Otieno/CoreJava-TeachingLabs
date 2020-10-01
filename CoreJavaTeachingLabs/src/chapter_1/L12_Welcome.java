f4d9e2601aaf91aaf9da3981d72c88d7c6577528
ADMIN <ADMIN@Academy>
2020-10-01 16:27:06 -0500

    Displaying multiple messages on the console

---
+++ CoreJavaTeachingLabs/src/chapter_1/L12_Welcome.java
@@ -0,0 +1,16 @@
+package chapter_1;
+
+public class L12_Welcome {
+	public static void main(String[] args) {
+		System.out.println("Programming is fun!");
+		System.out.println("Fundamentals First");
+		System.out.println("Problem Driven");
+	}
+}
+
+// NOTES
+
+/* The program L11_Welcome displays one message.
+ * Once you understand the program, it is easy to extend it to display more messages. 
+ * For example, you can rewrite the program to display three messages, as shown in L12_Welcome.
+ */

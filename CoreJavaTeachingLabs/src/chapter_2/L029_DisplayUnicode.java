d71aa354925e3060560d1125d6298935f6a9bc44
ADMIN <ADMIN@Academy>
2020-10-01 18:10:19 -0500

    JOptionPane.showMessageDialog - Display Unicode

---
+++ CoreJavaTeachingLabs/src/chapter_2/L029_DisplayUnicode.java
@@ -0,0 +1,14 @@
+package chapter_2;
+
+import javax.swing.JOptionPane;
+
+public class L029_DisplayUnicode {
+
+	public static void main(String[] args) {
+		
+		JOptionPane.showMessageDialog(null, "\u6B22\u8FCE \u03b1 \u03b2 \u03b3",
+				"\u6B22\u8FCE Welcome",JOptionPane.INFORMATION_MESSAGE);
+
+	}
+
+}
